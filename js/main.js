34c232a109de249a3d3da4da7620fbba7a7649d2
wladislaw353 <wladislaw353@gmail.com>
2022-04-10 00:50:28 +0200

    creative section animation complete

--- js/main.js
+++ js/main.js
@@ -101,9 +101,11 @@ $(document).ready(()=> {const wwt=1;function is_home(){return($('body').hasClass
             // активируем окно фрейма
             if (scrolledIntoCreative >= viewport) {
                 $creativeIframe.addClass('active')
-                console.log(1 - scrolledIntoCreative / 1600);
-                $creativeIframe.css('transform', `scale(${1 - scrolledIntoCreative / 1200})`)
+                setTimeout(() => {
+                    $creativeIframe.addClass('scrolled')
+                }, 300)
             } else {
+                $creativeIframe.removeClass('scrolled')
                 $creativeIframe.removeClass('active')
             }
 
@@ -111,59 +113,14 @@ $(document).ready(()=> {const wwt=1;function is_home(){return($('body').hasClass
             if (scrolledTemp < scrolled) {
                 $fixedImg.css('position', 'fixed')
             } else {
-                if (scrolled < pageHeight - viewport * 2 - 1000) {
+                if (scrolled < pageHeight - viewport * 2 - 300) {
                     $fixedImg.css('position', 'absolute')
                 }
             }
         }
         
     })
-    
-    // === lastest window animation
-    const $headImage        = document.querySelector('.fixed-img > .bg')
-    const $headTitle        = document.querySelector('.fixed-img > .content > h2')
-    const $iframeContainer  = document.querySelector('.iframe-wrapper')
-    const $iframe           = document.querySelector('.iframe-wrapper iframe')
-    let   $iframeHeader     = document.querySelector('.iframe-wrapper .window-header')
 
-    const headImageInitScale = 2
-
-    let headImageScaleDelta  = headImageInitScale - 1
-    let headImageCurentScale = headImageInitScale
-    let titleCurentOpacity   = 0
-    let titleCurentTransform = 0
-    let iframeScale          = 1
-
-    function setView(scrolled) {
-        // if (scrolled < viewport) {
-        //     headImageCurentScale = headImageInitScale - (headImageScaleDelta / viewport * scrolled)
-        //     titleCurentOpacity =  scrolled / 200
-        //     titleCurentTransform = 0 - (scrolled / 2)
-        //     $iframeContainer.classList.remove('active')
-        //     $iframe.style.cssText = `height: 100%; transform: translateY(0)`
-        //     $iframeContainer.style.cssText = `transform: scale(1)`
-        // } else {
-            headImageCurentScale = 1
-            titleCurentOpacity = 50
-            titleCurentTransform = viewport / 2 + 54
-            $iframeContainer.classList.add('active')
-            $iframe.style.cssText = `height: calc(100% - ${$iframeHeader.height}px); transform: translateY(${$iframeHeader.height}px)`
-            const scale = 1 - (scrolled / (viewport * 3)) + 0.3
-            if(scale > 0.76 && scale < 1) {
-                iframeScale = scale
-                $iframeContainer.classList.remove('scrolled')
-            }
-            if(scale <= 0.76) {
-                $iframeContainer.classList.add('scrolled')
-            }
-            // console.log(1 - (scrolled / (viewport * 3)) + 0.3)
-            $iframeContainer.style.cssText = `transform: scale(${iframeScale})`
-        // }
-
-        $headImage.style.cssText = `transform: scale(${headImageCurentScale})`
-        $headTitle.style.cssText = `filter: blur(${titleCurentOpacity}px); transform: translateY(${titleCurentTransform}px)`
-    }
-    // =====
 
 
     $('.filter button').click(function() {
